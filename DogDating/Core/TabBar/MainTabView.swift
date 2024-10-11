8e79385fcbd204e889a8fb1335103979b407a429
Jimy <jimybombela@yahoo.com>
2024-10-10 18:40:08 -0700

    Initial

--- DogDating/Core/TabBar/MainTabView.swift
+++ DogDating/Core/TabBar/MainTabView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainTabView: View {
     var body: some View {
         TabView {
-            Text("Swiping")
+            Text("Swipe")
                 .tabItem {
                     Image(systemName: "pawprint")
                         .imageScale(.large)
