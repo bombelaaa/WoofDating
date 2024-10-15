445870d86c24ddbebf13959196aaaee987cd52c7
Jimy <jimybombela@yahoo.com>
2024-10-14 20:22:46 -0700

    Addded Swipe Action Indicator

---
+++ DogDating/Core/Cards/Views/SwipeActionIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeActionIndicatorView.swift
+//  DogDating
+//
+//  Created by Jimy on 10/14/24.
+//
+
+import SwiftUI
+
+struct SwipeActionIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeActionIndicatorView()
+}
---
+++ DogDating/Utils/Constants/SizeConstants.swift
@@ -0,0 +1,18 @@
+//
+//  SizeConstants.swift
+//  DogDating
+//
+//  Created by Jimy on 10/14/24.
+//
+
+import SwiftUI
+
+struct SizeConstants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SizeConstants()
+}
