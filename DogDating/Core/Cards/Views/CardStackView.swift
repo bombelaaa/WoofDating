ecbe155d7f7d0c023065fc132e3f079143c613e4
Jimy <jimybombela@yahoo.com>
2024-12-10 20:33:13 -0800

    Stopped before adding Card Removal

---
+++ DogDating/Core/Cards/Views/CardStackView.swift
@@ -0,0 +1,18 @@
+//
+//  CardStackView.swift
+//  DogDating
+//
+//  Created by Jimy on 12/10/24.
+//
+
+import SwiftUI
+
+struct CardStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardStackView()
+}
---
+++ DogDating/Shared/Users/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  DogDating
+//
+//  Created by Jimy on 12/10/24.
+//
+
+import Foundation
