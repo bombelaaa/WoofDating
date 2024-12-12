b726f0d1560df0d59f66a1fce3b7141b5d1b1dd0
Jimy <jimybombela@yahoo.com>
2024-12-11 22:42:11 -0800

    Stopped before current user profile

---
+++ DogDating/Core/Profile/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  DogDating
+//
+//  Created by Jimy on 12/11/24.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
